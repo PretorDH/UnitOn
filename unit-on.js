74906cac1c237056a59641e3f87bc3152cc8b540
PretorDH <pretordh@deparadox.com>
2013-08-04 02:51:19 +0300

    First public version
    Basic functionality.
    Provides automatic script load on demand.

---
+++ unit-on.js
@@ -0,0 +1,55 @@
+﻿/**
+ * jQuery.Unit-On v0.1.3
+ * @author Dark Heart aka PretorDH
+ * @site uniton.deparadox.com
+ * MIT license
+ */
+
+(function(o){
+(Unit = function (o) {
+	this.U = o;
+	this.unitLoad = function (h,c) {
+		var Su=Unit.U[h];
+		if (Su.loaded) return;
+		if (Su.loaded===false && typeof c=='function') {
+			var a=Su.onload;
+			return Su.onload = function (){a && a();c && c()};
+		};
+		Su.onload = c;
+		if (Su.css) Unit.cssLoad(Su,(Su.jss)?null:Su.onload);
+		if (Su.jss) Unit.jssLoad(Su);
+		Su.loaded = false;
+	};
+	this.cssLoad = function (Su,c) {
+		var a,f=document.createElement("link");
+		(a=f.setAttribute)("rel", "stylesheet");
+		a("type", "text/css");
+		a("onload", function(){Su.onload && Su.onload()});
+		a("href", Su['css']);
+		(typeof f != "undefined") && document.getElementsByTagName("head")[0].appendChild(f) && console.log('Load:'+h['css']);
+	};
+	this.jssLoad = function (Su) {
+		$.getScript(Su['jss'],function(e){console.log('Load: '+Su['jss']); Su.onload && Su.onload(); Su.onload=null; Su.loaded=true});
+	}
+
+	for(var h in this.U) {
+		var nfn = this.U[h].fn;
+		if (nfn == 'NOW') this.unitLoad(h,this.U[h].onload);
+		if (nfn == 'ONLOAD') 
+			(function(h){
+				$(function(){Unit.unitLoad(h,Unit.U[h].onload);});
+			})(h);
+		if (typeof nfn == 'string') 
+			(function(nfn,h){
+				window[nfn] = function(){
+					var self=this, ar=arguments; 
+					Unit.unitLoad(h,function(){window[nfn].apply(self,ar) }); 
+				};
+			})(nfn,h);
+	};
+	return o;
+}).call(Unit,o);
+})
+
+({  'dragon':{jss:'http://dragon.deparadox.com/drag-on.js', fn:'NOW'},
+    'baron':{jss:'http://dragon.deparadox.com/bar-on.js', fn:'ONLOAD'}      });
